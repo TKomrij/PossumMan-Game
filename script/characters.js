92147ee933ae4ec534d6e5102ad3bfb326bf93da
Tatiane Komrij <tatianekomrij@Tatianes-MacBook-Pro.local>
2019-07-11 15:50:18 +0200

    Third commit

--- script/characters.js
+++ script/characters.js
@@ -114,7 +114,7 @@ let police = new createObject({
   y: canvas.height - 150,
   width: 90,
   height: 45,
-  speed: 15,
+  speed: 5,
 })
 
 ///////
@@ -180,6 +180,22 @@ function movement(possum) {
 }
 
 
+//movement police
+function chase(police) {
+  if(possum.x < police.x){
+    police.x -= police.speed;
+  }
+  if(possum.x > police.x){
+    police.x += police.speed;
+  }
+  if(possum.y < police.y){
+      police.y -= police.speed;
+  }
+  if(possum.y > police.y){
+    police.y += police.speed;
+  }
+}
+
 // Collisions
 // - Collision objects
 function collisionObjects(obj1, obj2) {
@@ -190,16 +206,17 @@ function collisionObjects(obj1, obj2) {
     && obj1.y < obj2.y + obj2.height
     && obj1.y + obj1.height > obj2.y) {
        //debugger
-      collision = true;
+      //collision = true;
       // if(collision == true) {
         livesLeft --
         // update scoreboard
         console.log("Oh no! You got caught!");
             livesLeft --;
             document.getElementById("lives").innerText = livesLeft;
-            return
-      // };
-      
+            if(livesLeft == 0) {
+              alert("Oh no! You got caught!")
+            };
+              return
     };
 
   // collision possum - food
@@ -213,24 +230,23 @@ function collisionObjects(obj1, obj2) {
       && badFood.indexOf(obj2[i]) == -1) {
         removeFood = obj2[i].id
         badFood.push(obj2[i])
-        debugger
+        //debugger
         collision = true;
         // if(collision == true) {
           // update scoreboard
           console.log("you got trash! Good job!");
             score ++;
             document.getElementById("eaten").innerText = score
+            if(score == 50) {
+              alert("You're a rootin' and tootin' possum!")
+            }
             return
-        // };
+        
        
       };
     };
   };
-}
-
-  
-
-
+};
 
 
 // - Collision Foods - Wall
@@ -273,37 +289,64 @@ function drawFood(foodImage, food) {
 function drawPolice(police) {
   ctx.drawImage(policeImage, police.x, police.y, police.width, police.height);
 }
+// filterfoods
+// badfood
+// removefood
+// foods 
 
 function draw () {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   drawPossum(possum);
-  if(collision == true) {
-    filterFoods = filterFoods.filter(food => !(food.id == removeFood))
-    for(i = 0; i < filterFoods.length; i++) {
-      drawFood(filterFoods[i].image, filterFoods[i]);
-    }
-    //debugger
-  } else {
-    for(i = 0; i < foods.length; i++) {
-      drawFood(foods[i].image, foods[i]);
-    }
+
+
+  filterFoods = filterFoods.filter(food => !(food.id == removeFood))
+
+  for(i = 0; i < filterFoods.length; i++) {
+    drawFood(filterFoods[i].image, filterFoods[i]);
   }
-  if(badFood.length > 0) {
-    setTimeout(function() {
-      console.log("ksjdfhskjdfhs")
-      //debugger
-      for(i = 0; i < badFood.length; i++) {
-        drawFood(badFood[i].image, badFood[i]);
+  if(collision) {
+    setTimeout(()=> {
+      let removedFood = foods.filter(food => (food.id == removeFood));
+      removeFood = undefined
+      debugger
+      badFood = [];
+      if(removedFood[0]) {
+        filterFoods.push(removedFood[0]);
       }
-      
-    }, 1000)
+    }, 2000)
   }
+  collision = false;
+
+
+  // if(collision == true) {
+  //   filterFoods = filterFoods.filter(food => !(food.id == removeFood))
+  //   for(i = 0; i < filterFoods.length; i++) {
+  //     drawFood(filterFoods[i].image, filterFoods[i]);
+  //   }
+  //   collision = false;
+  //   //debugger
+  // } else {
+  //   for(i = 0; i < foods.length; i++) {
+  //     drawFood(foods[i].image, foods[i]);
+  //   }
+  // }
+  // if(badFood.length > 0) {
+  //   setTimeout(function() {
+  //     console.log("ksjdfhskjdfhs")
+  //     //debugger
+  //     for(i = 0; i < badFood.length; i++) {
+  //       drawFood(badFood[i].image, badFood[i]);
+  //     }
+      
+  //   }, 1000)
+  // }
   drawPolice(police);
 }
 
 function updateCanvas() {
     movement(possum)
     draw();
+    chase(police);
 
     collisionObjects(possum, foods);
     collisionObjects(possum, police);
@@ -342,306 +385,6 @@ updateCanvas();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// var canvas = document.getElementById("board");
-// var ctx    = canvas.getContext("2d");
-// ctx.fillStyle = 'white';
-// ctx.font = '18px serif';
-
-// var keys = {};
-// var score = 0;
-// var livesLeft = 3;
-// var collision = false;
-// var collision2 = false;
-
-// canvas.width = window.innerHeight;
-// canvas.height = window.innerHeight
-
-// // Images
-// const possumImage = new Image();
-// possumImage.src = "https://media.giphy.com/media/xT0BKuZRyjpUQadEpq/giphy.gif";
-
-// const foodImage1 = new Image();
-// foodImage1.src = "https://media.giphy.com/media/dYwyN5xXBP5hSP77bo/giphy.gif";
-
-// const foodImage2 = new Image()
-// foodImage2.src = "https://media.giphy.com/media/1qfKSo9vsdZh70D4ZT/giphy.gif";
-
-// const foodImage3 = new Image();
-// foodImage3.src = "https://media.giphy.com/media/cLZYVIRbpI6TsemLFw/giphy.gif";
-
-// const foodImage4 = new Image();
-// foodImage4.src = "https://media.giphy.com/media/xUOwFRUH1foa66zvLq/giphy.gif";
-
-// const policeImage = new Image();
-// policeImage.src = "https://media.giphy.com/media/l0Iy8RHyESNYq2FcA/giphy.gif";
-
-
-// function createObject (object) {
-//   this.x = object.x;
-//   this.y = object.y;
-//   this.width = object.width;
-//   this.height = object.height;
-//   this.speed = object.speed;
-//   this.gravity = object.gravity
-// }
-
-// // possum
-// let possum = new createObject ({
-//   x: 25,
-//   y: 25,
-//   width: 90,
-//   height: 45,
-//   speed: 12,
-// })
-
-// var dx = 2;
-// var dy = -2;
-
-
-// // foods
-// let foods = []
-
-// let food1 = new createObject ({
-//   x: Math.floor(Math.random() * (canvas.width - 70)),
-//   y: Math.floor(Math.random()* (canvas.height - 70)),
-//   width: 90,
-//   height: 45,
-//   speed: 3,
-//   gravity: 0.5
-// })
-
-// let food2 = new createObject ({
-//   x: Math.floor(Math.random() * (canvas.width - 70)),
-//   y: Math.floor(Math.random()* (canvas.height - 70)),
-//   width: 90,
-//   height: 45,
-//   speed: 4,
-//   gravity: 0.5
-// })
-
-// let food3 = new createObject ({
-//   x: Math.floor(Math.random() * (canvas.width - 70)),
-//   y: Math.floor(Math.random()* (canvas.height - 70)),
-//   width: 90,
-//   height: 45,
-//   speed: 5,
-//   gravity: 0.5
-// })
-
-// let food4 = new createObject ({
-//   x: Math.floor(Math.random() * (canvas.width - 70)),
-//   y: Math.floor(Math.random()* (canvas.height - 70)),
-//   width: 90,
-//   height: 45,
-//   speed: 5,
-//   gravity: 0.5
-// })
-
-// foods.push(food1, food2, food3, food4)
-
-// // police
-// let police = new createObject({
-//   x: canvas.width - 100,
-//   y: canvas.height - 150,
-//   width: 90,
-//   height: 45,
-//   speed: 15,
-// })
-
-// ///////
-// ////////////////////
-// // Done:
-// // - make different trash objects appear on canvas (V)
-// // - move trash objects in random positions
-// // - add police (V)
-
-// // To Do Tatiane:
-// // - dissappear of trash if the collision is true
-// // - updatescoreboard/lives
-// // - police follow possum
-// // - police stay within boundary
-// // - trash stay within boundary
-
-// // Movement
-// window.addEventListener("keydown", function (e) {
-//   keys[e.keyCode] = true;
-//   e.preventDefault();
-// })
-
-// window.addEventListener("keyup", function (e) {
-//   delete keys[e.keyCode];
-// })
-
-// function movement(possum) {
-//   // left
-//   if (37 in keys) {
-//       if(possum.x - possum.speed > 0) {
-//           possum.x -= possum.speed;
-//       }
-//       possum.direction = 'left';
-//   }
-//   // up
-//   if (38 in keys) {
-//       if(possum.y - possum.speed > 0) {
-//           possum.y -= possum.speed;
-//       }
-//       possum.direction = 'up'
-//   }
-//   // right
-//   if (39 in keys) {
-//       if(possum.x + possum.width +  possum.speed < canvas.width) {
-//           possum.x += possum.speed;
-//       }
-//       possum.direction = 'right';
-//   }
-//   // down
-//   if (40 in keys) {
-//       if(possum.y + possum.height + possum.speed < canvas.height) {
-//           possum.y += possum.speed;
-//       }
-//       possum.direction = 'down';
-//   }
-// }
-// function movementPolice(police) {
-//   police.x = possum.x && police.y == possum.x
-// }
-
-// function update() {
-//   movement(possum)
-
-// }
-
-// // Scoreboard
-// function updateScoreBoard() {
-//   // scoreboard logic
-//   if(collision == true) {
-//     document.getElementById("eaten").innerHTML = score;
-//     score ++;
-//   }
-//   else (collision2 == true) {
-//     document.getElementById("eaten").innerHTML = score;
-//     score -= 1
-//   }
-//   }
-  
-  
-
-
-// // Collision Possum vs Food
-// function collisionObjects(possum, food) {
-//   if(possum.x < food.x + food.width
-//     && possum.x + possum.width > food.x
-//     && possum.y < food.y + food.height
-//     && possum.y + possum.height > food.y) {
-//       collision = true;
-//     }
-//       // call scoreboard 
-//       updateScoreBoard()  
-// }
-
-
-// // Collision Possum vs Police
-// function collisionObjects2(possum, police) {
-//   if(possum.x < police.x + police.width
-//     && possum.x + possum.width > police.x
-//     && possum.y < police.y + police.height
-//     && possum.y + possum.height > police.y) {
-//       collision2 = true;
-//   }
-// }        
-  
-
-    
-
-
-
-// function slowMoveFood (food1, food2, food3, food4) {
-  
-//   food1.x += food1.speed;
-//   food1.y += food1.gravity;
-
-//   food2.x -= food2.speed;
-//   food2.y += food2.gravity;
-
-//   food3.y -= food3.speed;
-//   food3.y += food3.gravity;
-
-//   food4.y -= food4.speed;
-//   food4.y += food4.gravity;
-// }
-
-// // function fastMovePolice (police) {
-// //   police.x -= police.speed;
-// //   police.y -= police.speed;
-
-// //   if(police.x - police.speed > 0) {
-// //     police.x -= police.speed;
-// //   }
-// //   police.direction = 'left';
-
-
-// //   if(police.y - police.speed > 0) {
-// //   police.y -= police.speed;
-// //   }
-// //   police.direction = 'up'
-  
-
-// //   if(police.x + police.width  +  police.speed < canvas.width) {
-// //     police.x += police.speed;
-// // }
-// // police.direction = 'right';
-
-// // if(police.y + police.height + police.speed < canvas.height) {
-// //   police.y += police.speed;
-// // }
-// // police.direction = 'down';
-// // }
-
-
-
-
-// // Draw Images
-// function drawPossum(possum) {
-//   ctx.drawImage(possumImage, possum.x, possum.y, possum.width, possum.height)
-// }
-// function drawFood(foodImage, food) {
-//   ctx.drawImage(foodImage, food.x, food.y, food.width, food.height)
-// }
-// function drawPolice(police) {
-//   ctx.drawImage(policeImage, police.x, police.y, police.width, police.height)
-// }
-
-
-// function draw () {
-//   ctx.clearRect(0,0, canvas.width, canvas.height)
-//   drawPossum(possum)
-//   drawFood(foodImage1, food1)
-//   drawFood(foodImage2, food2)
-//   drawFood(foodImage3, food3)
-//   drawFood(foodImage4, food4)
-//   drawPolice(police)
-//   collisionObjects(possum, foods)
-//   slowMoveFood(food1, food2, food3, food4)
-//   // fastMovePolice(police)
-// }
-
-// function updateCanvas() {
-//     update()
-//     draw()
-//     window.requestAnimationFrame(updateCanvas)
-// }
-// updateCanvas()
 
 
 
